84dee61593ef06ccc8a5a0bf05fc99190b76923f
kaio16 <seuemail@gmail.com>
2026-05-22 20:07:39 -0300

    novo layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import { useState } from "react";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-
 export default function App() {
-  const [user, setUser] = useState(localStorage.getItem("user"));
-
-  function handleLogin(email) {
-    setUser(email);
-  }
-
-  function handleLogout() {
-    localStorage.removeItem("user");
-    setUser(null);
-  }
-
   return (
-    <>
-      {user ? (
-        <Dashboard user={user} onLogout={handleLogout} />
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
-    </>
-  );
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Meu App Profissional 🚀
+      </h1>
+    </div>
+  )
 }
