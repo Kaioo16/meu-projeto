e909f7d5e5e8208066a8e78721958ab62893b6c2
kaio16 <seuemail@gmail.com>
2026-05-22 20:23:29 -0300

    tailwind funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-700">
         Meu App Profissional 🚀
       </h1>
     </div>
